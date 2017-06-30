1c09ce860af6338e82d44f955c05d880b09d6378
zackattacknyu <zackattacknyu@gmail.com>
2017-06-29 21:52:21 -0700

    initialized Java project

---
+++ DataEngChallenge/src/dataengchallenge/DataEngChallenge.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataengchallenge;
+
+/**
+ *
+ * @author Zach
+ */
+public class DataEngChallenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
