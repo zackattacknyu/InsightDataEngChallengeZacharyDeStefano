241965cfb74140bba04dd22d3240ffe9a3ad366c
zackattacknyu <zackattacknyu@gmail.com>
2017-07-02 15:59:03 -0700

    json file test started

---
+++ DataEngChallenge/src/zrdchallengemain/JsonFileTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zrdchallengemain;
+
+/**
+ *
+ * @author Zach
+ */
+public class JsonFileTest {
+    
+}
