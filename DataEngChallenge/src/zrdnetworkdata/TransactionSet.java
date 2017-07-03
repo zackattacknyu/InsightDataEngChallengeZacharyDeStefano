c744c2e0731ff6f68c34acf8a48aa85897886149
zackattacknyu <zackattacknyu@gmail.com>
2017-07-02 19:39:33 -0700

    more

--- DataEngChallenge/src/zrdnetworkdata/TransactionSet.java
+++ DataEngChallenge/src/zrdnetworkdata/TransactionSet.java
@@ -21,31 +21,52 @@ public class TransactionSet {
      */
     
     public HashMap<Long,Integer> lastTransNumberUsed;
+    public HashMap<Long,Integer> numTransPerTime;
     
     public TreeSet<Transaction> transSet;
     
-    private void addToSet(User userX,long transTime,double amount){
-        int transNumberUse=0;
-        if(lastTransNumberUsed.containsKey(transTime)){
-            transNumberUse=lastTransNumberUsed.get(transTime);
-        }
-        
-        transNumberUse++;
-        
-        addToSet(new Transaction(transTime,transNumberUse,amount,userX));
-        
-        lastTransNumberUsed.put(transTime,transNumberUse);
+    public TransactionSet(int maxSize){
+        this.maximumSize=maxSize;
+        transSet = new TreeSet<>();
+        lastTransNumberUsed = new HashMap<>();
     }
     
-    private void addToSet(Transaction trans){
+    public void addToSet(User userX,long transTime,double amount){
+        int transNumber = addToHashMap(lastTransNumberUsed,transTime);
+        addToHashMap(numTransPerTime,transTime);
         
+        Transaction trans=new Transaction(transTime,transNumber,amount,userX);
         if(currentSize < maximumSize){
             currentSize++;
         }else{
-            transSet.pollLast();
+            removeEarliest();
         }
         transSet.add(trans);
         
     }
+    
+    private int addToHashMap(HashMap<Long,Integer> toAdd, long transTime){
+        int numberUse=0;
+        if(toAdd.containsKey(transTime)){
+            numberUse=toAdd.get(transTime);
+        }
+        numberUse++;
+        toAdd.put(transTime,numberUse);
+        return numberUse;
+    }
+    
+    private void removeEarliest(){
+        Transaction lastOne = transSet.pollFirst();
+        long transTime = lastOne.getTransactionTime();
+        if(numTransPerTime.get(transTime) < 2){
+            numTransPerTime.remove(transTime);
+            lastTransNumberUsed.remove(transTime);
+        }else{
+            int currentNumTrans = numTransPerTime.get(transTime);
+            numTransPerTime.put(transTime, currentNumTrans-1);
+        }
+    }
+    
+
     
 }
