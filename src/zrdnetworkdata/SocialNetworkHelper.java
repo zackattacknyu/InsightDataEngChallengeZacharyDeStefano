d92f850a8bc0df7a6711abab633e3ef47eb5041b
zackattacknyu <zackattacknyu@gmail.com>
2017-07-04 18:28:03 -0700

    more comments

--- src/zrdnetworkdata/SocialNetworkHelper.java
+++ src/zrdnetworkdata/SocialNetworkHelper.java
@@ -7,24 +7,34 @@ package zrdnetworkdata;
 import java.util.HashSet;
 
 /**
+ * Helper class that implements some social network functionality
  *
  * @author Zach
  */
 public class SocialNetworkHelper {
+    
+    /**
+     * "D" parameter value
+     */
     public static int numDegreesSocialNetwork=2;
     
+    /**
+     * Does unfriending between user number 1 and user number 2
+     * @param user1 user number 1 
+     * @param user2 user number 2
+     */
     public static void endFriendship(User user1, User user2){
-        /*
-         * Code here will unfriend the two people
-         */
+        
         user1.removeNeighbor(user2);
         user2.removeNeighbor(user1);
     }
     
+    /**
+     * Does friending between user 1 and user 2
+     * @param user1 user number 1
+     * @param user2 user number 2
+     */
     public static void beginFriendship(User user1, User user2){
-        /*
-         * Code here will link two people
-         */
         user1.addNeighbor(user2);
         user2.addNeighbor(user1);
     }
@@ -32,14 +42,16 @@ public class SocialNetworkHelper {
     /**
      * Obtains the social network of the starting users
      *      Returns a set that DOES NOT include the starting users
-     * @param startingUsers
-     * @return
+     * @param startingUsers set of users that are the base nodes
+     * @return  set representing the social network
      */
     public static HashSet<User> obtainSocialNetwork(HashSet<User> startingUsers) {
         HashSet<User> usersInNetwork = new HashSet<>();
         HashSet<User> currentNodes = startingUsers;
         HashSet<User> neighborsOfCurrent;
         usersInNetwork.addAll(startingUsers);
+        
+        //the BFS search is done in this loop
         for (int j = 0; j < numDegreesSocialNetwork; j++) {
             neighborsOfCurrent = new HashSet<>();
             for (User node : currentNodes) {
@@ -52,17 +64,21 @@ public class SocialNetworkHelper {
             }
             currentNodes = neighborsOfCurrent;
         }
+        
+        //make sure starting users are not included
         usersInNetwork.removeAll(startingUsers);
+        
         return usersInNetwork;
     }
 
     /**
      * Does a breadth-first search to obtain the users
      *      whose transaction log needs to be updated
-     *      by the change in the social network
-     * @param user1
-     * @param user2
-     * @return
+     *      by the change in the social network. 
+     * Result INCLUDES the original users too
+     * @param user1     user number 1
+     * @param user2     user number 2
+     * @return  users who need to be updated
      */
     public static HashSet<User> obtainUsersToUpdate(User user1, User user2) {
         HashSet<User> startingUsers = new HashSet<User>();
@@ -73,6 +89,12 @@ public class SocialNetworkHelper {
         return usersInNetwork;
     }
 
+    /**
+     * Obtains a user's social network. Result DOES NOT INCLUDE the original
+     *      user
+     * @param user1     user to obtain network for
+     * @return      set of users in their network
+     */
     public static HashSet<User> obtainSocialNetworkForUser(User user1) {
         HashSet<User> userSet = new HashSet<User>();
         userSet.add(user1);
--- src/zrdnetworkdata/TimestampHelper.java
+++ src/zrdnetworkdata/TimestampHelper.java
@@ -7,11 +7,17 @@ package zrdnetworkdata;
 import java.sql.Timestamp;
 
 /**
- *
+ * Methods to help with handling the timestamps
  * @author Zach
  */
 public class TimestampHelper {
     
+    /**
+     * Takes the timestamp from the Json files and returns 
+     *      milliseconds since 1970
+     * @param timestamp     timestamp string from Json file
+     * @return  millis since 1970 long
+     */
     public static long getMillisTime(String timestamp){
         Timestamp currentTime = Timestamp.valueOf(timestamp);
         return currentTime.getTime();
